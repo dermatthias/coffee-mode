1c069876b5213eafb04599934dcc94cadcdd80af
Matthias Schneider <matthias.schneider@beached.org>
2011-04-13 22:05:28 +0200

    changed default key modifier from (apple) A- to C-

--- coffee-mode.el
+++ coffee-mode.el
@@ -568,9 +568,9 @@ line? Returns `t' or `nil'. See the README for more details."
   "Major mode for editing CoffeeScript..."
 
   ;; key bindings
-  (define-key coffee-mode-map (kbd "A-r") 'coffee-compile-buffer)
-  (define-key coffee-mode-map (kbd "A-R") 'coffee-compile-region)
-  (define-key coffee-mode-map (kbd "A-M-r") 'coffee-repl)
+  (define-key coffee-mode-map (kbd "C-c C-b") 'coffee-compile-buffer)
+  (define-key coffee-mode-map (kbd "C-c C-r") 'coffee-compile-region)
+  (define-key coffee-mode-map (kbd "C-c !") 'coffee-repl)
   (define-key coffee-mode-map [remap comment-dwim] 'coffee-comment-dwim)
   (define-key coffee-mode-map "\C-m" 'coffee-newline-and-indent)
 
